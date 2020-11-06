ab1e346ff1c059811092a7d51803b9c8aee8c048
Henry <shanchuan.he@controlsolutions.nl>
2020-11-06 11:15:36 +0100

    add the api server

---
+++ http_channel_listener_api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import yaml
+import urllib.request
+from flask_restful import Resource, Api
+
+# set up the flask module
+app = Flask(__name__)
+
+# import settings
+with open(r'.cfg.yml') as file:
+    cfg = yaml.safe_load(file)
+
+url = cfg['url']
+trigger_string = 'minimaal 10 dagen'
+tsb = trigger_string.encode()
+
+# when visit localhost:5000 what do you want it to return?
+@app.route('/')
+def hello():
+    page = urllib.request.urlopen(url)
+    load_page = page.read()
+    if tsb in load_page:
+        return "True"
+    return "False"
+# main function do python app.py to run it
+if __name__ == '__main__':
+    app.run()
