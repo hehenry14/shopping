66e6d02100340902981b3d7bd702f0286ea9b299
Henry <shanchuan.he@controlsolutions.nl>
2020-11-07 10:54:29 +0100

    updated the keyboard interruptions

--- main.py
+++ main.py
@@ -14,39 +14,56 @@ with open(r'.cfg.yml') as file:
     cfg = yaml.safe_load(file)
 
 have_purchased = False
-while not have_purchased:
-    page = urllib.request.urlopen('http://127.0.0.1:5000/')
 
-    page.read() # without this line it won't work, dont know why yet.
-    have_stock = bool(page.read())
-    # log and show the status of the program
-    status = str(datetime.now()) + ' checking whether it is in stock, result is: ' + str(have_stock)
-    logging.debug(status)
+# config release date:
+release_date = datetime(2020, 11, 18, 12, 0, 6, 0)
 
-    # if have stock, make purchase
-    if have_stock:
-        have_purchased = True
-        # load website
-        browser = webdriver.Chrome(cfg['webdriver'])
-        browser.get(cfg['url'])
+print('Press ctl + c to stop the program')
+try:
+    while not have_purchased:
+        if datetime.now() < release_date:
+            logging.debug(str(datetime.now()) + ': the product have not yet been released')
+        else:
+            have_stock = False
+            try:
+                page = urllib.request.urlopen('http://127.0.0.1:5000/')
 
-        # click the button to log in
-        python_button = browser.find_elements_by_xpath(cfg['xpath_find_log_in'])[0]
-        python_button.click()
+                page.read() # without this line it won't work, dont know why yet.
+                have_stock = bool(page.read())
+                status = str(datetime.now()) + ' checking whether it is in stock, result is: ' + str(have_stock)
+                logging.debug(status)
+            except Exception as e:
+                logging.warning(str(datetime.now()) + ': localhost:5000 is not available')
+            # log and show the status of the program
 
-        # fill in the user name
-        browser.find_elements_by_xpath(cfg['xpath_user_name'])[0].send_keys(cfg['username'])
-        # put delay to avoid bug
-        time.sleep(2.5)
 
-        # fill in password
-        b = browser.find_elements_by_xpath(cfg['xpath_password'])[0]
-        b.send_keys(cfg['password'])
+            # if have stock, make purchase
+            if have_stock:
+                have_purchased = True
+                # load website
+                browser = webdriver.Chrome(cfg['webdriver'])
+                browser.get(cfg['url'])
 
-        # click log in
-        login_button = browser.find_elements_by_xpath(cfg['xpath_login_click'])[0]
-        login_button.click()
-        browser.close()
+                # click the button to log in
+                python_button = browser.find_elements_by_xpath(cfg['xpath_find_log_in'])[0]
+                python_button.click()
 
-    # pause every 60 to 120 seconds
-    time.sleep(60 + random.randint(0, 60))
+                # fill in the user name
+                browser.find_elements_by_xpath(cfg['xpath_user_name'])[0].send_keys(cfg['username'])
+                # put delay to avoid bug
+                time.sleep(2.5)
+
+                # fill in password
+                b = browser.find_elements_by_xpath(cfg['xpath_password'])[0]
+                b.send_keys(cfg['password'])
+
+                # click log in
+                login_button = browser.find_elements_by_xpath(cfg['xpath_login_click'])[0]
+                login_button.click()
+                #browser.close()
+
+        # pause every 60 to 120 seconds
+        time.sleep(30 + random.randint(0, 3))
+except KeyboardInterrupt:
+    print('exited')
+    pass
